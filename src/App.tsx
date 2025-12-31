242a51a3cb35362f400eba000679e3426e043dad
Harish143-dev <saravananharish143@gmail.com>
2025-12-31 10:27:45 +0530

    fff

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/edu-marketing">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/thank-you" element={<ThankYou />} />
